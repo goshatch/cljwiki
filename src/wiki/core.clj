96d0af8878ea5592aef99ea908326a0c4aa1a26c
Gosha Tcherednitchenko <git@gosha.net>
2024-07-19 17:24:54 +0100

    Refactor routing based on simple-router README

--- src/wiki/core.clj
+++ src/wiki/core.clj
@@ -1,7 +1,7 @@
 (ns wiki.core
-  (:require [ring.adapter.jetty :refer [run-jetty]]
-            [ring.middleware.params :refer [wrap-params]]
-            [ring.util.response :refer [response]]
+  (:require [ring.adapter.jetty :as jetty]
+            [ring.middleware.params :as params]
+            [ring.util.response :as response]
             [clojure.java.io :as io]
             [clj-simple-router.core :as router])
   (:gen-class))
@@ -18,35 +18,49 @@
 
 (defn render-page [page-name]
   (if-let [content (read-page page-name)]
-    (response (str "<html><body><h1>" page-name "</h1><p>" content "</p></body></html"))
-    (response (str "<html><body><h1>Create: " page-name "</h1><form method='post'><textarea name='content'></textarea><button type='submit'>Save</button></form></body></html>"))))
+    (response/response (str "<html><body><h1>" page-name "</h1><p>" content "</p></body></html"))
+    (response/response (str "<html><body><h1>Create: " page-name "</h1><form method='post'><textarea name='content'></textarea><button type='submit'>Save</button></form></body></html>"))))
 
 (defn save-page [page-name content]
   (write-page page-name content)
-  (response (str "<html><body><h1>" page-name " saved</h1><a href='/w/" page-name "'>View page</a></body></html>")))
+  (response/response (str "<html><body><h1>" page-name " saved</h1><a href='/w/" page-name "'>View page</a></body></html>")))
+
+;; (def routes
+;;   {"GET /"
+;;    (fn [_req]
+;;      {:status 200
+;;       :body "<html><body><h1>Welcome to the Wiki!</h1><a href='/w/Home'>Home</a></body></html>"})
+;;    "GET /w/*"
+;;    (fn [req]
+;;      (let [page-name (first (:path-params req))]
+;;        (render-page page-name)))
+;;    "POST /w/*"
+;;    (fn [req]
+;;      (let [page-name (first (:path-params req))
+;;            content (:content (:params req))]
+;;        (save-page page-name content)))})
 
 (def routes
-  {"GET /"
-   (fn [_req]
-     {:status 200
-      :body "<html><body><h1>Welcome to the Wiki!</h1><a href='/w/Home'>Home</a></body></html>"})
-   "GET /w/*"
-   (fn [req]
-     (let [page-name (first (:path-params req))]
-       (render-page page-name)))
-   "POST /w/*"
-   (fn [req]
-     (let [page-name (first (:path-params req))
-           content (:content (:params req))]
-       (save-page page-name content)))})
+  (router/routes
+    "GET /" []
+    {:status 200
+     :body "<html><body><h1>Welcome to the Wiki!</h1><a href='/w/Home'>Home</a></body></html>"}
+
+    "GET /w/*" [page-name]
+    (render-page page-name)
+
+    "POST /w/*" req
+    (let [page-name (first (:path-params req))
+          content (:content (:params req))]
+      (save-page page-name content))))
 
 (defn handler []
   (-> (router/router routes)
-      (wrap-params)))
+      (params/wrap-params)))
 
 
 (defn -main [& _args]
-  (run-jetty (handler) {:port 3001 :join? false}))
+  (jetty/run-jetty (handler) {:port 3001 :join? false}))
 
 (comment
   (-main))
